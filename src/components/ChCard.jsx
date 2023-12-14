6957c186aa9478cbcf4fb7517276dbb9dd23a16f
Nuruthantur <olivermayer123@gmail.com>
2023-12-14 10:47:20 +0100

    flipping works (flippidy flip)

--- src/components/ChCard.jsx
+++ src/components/ChCard.jsx
@@ -9,27 +9,27 @@ import '../styles/ChCard.scss'
 function ChCard({ character, handleShow }) {
 
   return (
-    // <div className="flip-card-outer">
-    //   <div className={"flip-card-inner"}>
-    //     <div className="card front">
-    //       <div className="card-body d-flex justify-content-center align-items-center">
-    //         <h1>{character.name}</h1>
-    //         <img src={character.image} alt={`Picture of ${character.name}`} />
-    //       </div>
-    //     </div>
-    //     <div className="card back">
-    //       <div className="card-body d-flex justify-content-center align-items-center">
-    //         <Button variant='primary' onClick={() => handleShow(character)}>Learn more!</Button>
-    //       </div>
-    //     </div>
-    //   </div>
-    // </div>
-
-      <div className="card-container" style={{ display: "flex", flexDirection: "column" }}>
-        <h1>{character.name}</h1>
-        <img src={character.image} alt={`Picture of ${character.name}`} />
-        <Button variant='primary' onClick={() => handleShow(character)}>Learn more!</Button>
+    <div className="flip-card">
+      <div className={"flip-card-inner"}>
+        <div className="flip-card-front">
+          <div className="card-body d-flex justify-content-center align-items-center">
+            <h1>{character.name}</h1>
+            <img src={character.image} alt={`Picture of ${character.name}`} />
+          </div>
+        </div>
+        <div className="flip-card-back">
+          <div className="card-body">
+            <Button variant='primary' onClick={() => handleShow(character)}>Learn more!</Button>
+          </div>
+        </div>
       </div>
+    </div>
+
+      // <div className="card-container" style={{ display: "flex", flexDirection: "column" }}>
+      //   <h1>{character.name}</h1>
+      //   <img src={character.image} alt={`Picture of ${character.name}`} />
+      //   <Button variant='primary' onClick={() => handleShow(character)}>Learn more!</Button>
+      // </div>
       
   )
 }
